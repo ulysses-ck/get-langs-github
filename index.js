e42ba1743d54f6b846558de349bac4d2046129e3
ulysses-ck <ulisesapaza.280@gmail.com>
2024-08-18 19:27:01 -0300

    fix: add try catch and conditionals

--- index.js
+++ index.js
@@ -6,39 +6,55 @@ app.use(express.json());
 
 app.get("/user/:user", async (req, res) => {
   const { user } = req.params;
-  const result = await fetch(`https://api.github.com/users/${user}/repos`);
-  const data = await result.json();
-  const languages = {};
-  data.forEach((e) => {
-    if (e.language) {
-      if (languages.hasOwnProperty(e.language)) {
-        languages[e.language]++;
-      } else {
-        languages[e.language] = 1;
-      }
+  try {
+    const result = await fetch(`https://api.github.com/users/${user}/repos`);
+    const data = await result.json();
+
+    if (data.status !== "200") {
+      res.json(data);
+
+			return;
     }
-  });
 
-  const totalRepos = Object.keys(languages).reduce(
-    (acc, curr) => languages[curr] + acc,
-    0
-  );
-  const languagesPercentage = {};
-  Object.keys(languages).forEach(
-    (e) =>
-      (languagesPercentage[e] = ((languages[e] * 100) / totalRepos).toFixed(2))
-  );
+    const languages = {};
+    data.forEach((e) => {
+      if (e.language) {
+        if (languages.hasOwnProperty(e.language)) {
+          languages[e.language]++;
+        } else {
+          languages[e.language] = 1;
+        }
+      }
+    });
 
-  let resultStr = [`Total of public repositories(that have a recognizable main language by GitHub): ${totalRepos}.`];
-
-  Object.keys(languagesPercentage)
-    .sort((a, b) => languages[b] - languages[a])
-    .forEach((e) =>
-      resultStr.push(
-        `${languagesPercentage[e]}% (${languages[e]}) of repos were made with ${e}.`
-      )
+    const totalRepos = Object.keys(languages).reduce(
+      (acc, curr) => languages[curr] + acc,
+      0
     );
-  res.json({ message: resultStr });
+    const languagesPercentage = {};
+    Object.keys(languages).forEach(
+      (e) =>
+        (languagesPercentage[e] = ((languages[e] * 100) / totalRepos).toFixed(
+          2
+        ))
+    );
+
+    let resultStr = [
+      `Total of public repositories(that have a recognizable main language by GitHub): ${totalRepos}.`,
+    ];
+
+    Object.keys(languagesPercentage)
+      .sort((a, b) => languages[b] - languages[a])
+      .forEach((e) =>
+        resultStr.push(
+          `${languagesPercentage[e]}% (${languages[e]}) of repos were made with ${e}.`
+        )
+      );
+    res.json({ message: resultStr });
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
 });
 
 app.listen(port, () => {
